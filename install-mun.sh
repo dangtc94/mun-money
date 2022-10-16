166557761c8ab854d9e458f71d50fbb243fbcaff
dieuts <dieutx90@gmail.com>
2022-10-16 20:36:17 +0700

    asdas

--- install-mun.sh
+++ install-mun.sh
@@ -6,7 +6,7 @@ sudo apt install ufw build-essential jq -y
 
 sleep 1
 
-if [[ $(which docker) && $(docker version) ]]; then
+if [[ $(which go) && $(go version) ]]; then
     echo "Go installed!"
     # command
   else
@@ -63,23 +63,30 @@ mund keys add dw --keyring-backend test
 # royal impact join glad company patch service choose sting enroll ribbon junk fragile lamp edit upper crumble timber parent surround squeeze banana penalty resist
 
 
-sleep 20
+sleep 1
+
+read -p "Do you back up the key? " yn
+    case $yn in
+        [Yy]* ) echo "next step..."; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 
 curl --tlsv1 https://node1.mun.money/genesis? | jq ".result.genesis" > ~/.mun/config/genesis.json
 
-# sleep 1
+sleep 1
 
 
-# word1="seeds = \"\""
-# word2='seeds = \"b4eeaf7ca17e5186b181885714cedc6a78d20c9b@167.99.6.48:26656,6a08f2f76baed249d3e3c666aaef5884e4b1005c@167.71.0.38:26656,9240277fca3bfa0c3b94efa60215ca10cf54f249@45.76.68.116:26656\"'
-# sed -i "s/$word1/$word2/g" ~/.mun/config/config.toml
+word1="seeds = \"\""
+word2='seeds = \"b4eeaf7ca17e5186b181885714cedc6a78d20c9b@167.99.6.48:26656,6a08f2f76baed249d3e3c666aaef5884e4b1005c@167.71.0.38:26656,9240277fca3bfa0c3b94efa60215ca10cf54f249@45.76.68.116:26656\"'
+sed -i "s/$word1/$word2/g" ~/.mun/config/config.toml
 
-# word3="laddr = \"tcp://127.0.0.1:26657\""
-# word4="laddr = \"tcp://0.0.0.0:26657\""
-# sed -i "s@$word3@$word4@g" ~/.mun/config/config.toml
-# sed -i 's/stake/utmun/g' ~/.mun/config/genesis.json
+word3="127.0.0.1:26657"
+word4="0.0.0.0:26657"
+sed -i "s/$word3/$word4/g" ~/.mun/config/config.toml
+sed -i 's/stake/utmun/g' ~/.mun/config/genesis.json
 
-# sleep 1
+sleep 1
 cat << 'EOF' >/etc/systemd/system/mund.service
 [Unit]
 Description=mund
@@ -108,15 +115,15 @@ WantedBy=multi-user.target
 
 EOF
 
-# sleep 1
-# make log-files
+sleep 1
+make log-files
 
-# sleep 1
-# sudo systemctl enable mund
-# sudo systemctl start mund
-# #sudo systemctl restart mund
+sleep 1
+sudo systemctl enable mund
+sudo systemctl start mund
+#sudo systemctl restart mund
 
-# sleep 1
-# mund status
+sleep 1
+mund status
 
 # journalctl -fu mund.service
