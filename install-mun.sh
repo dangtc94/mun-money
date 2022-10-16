273c7113f6237cf779d9b8ad5039eaed04c20a39
dieuts <dieutx90@gmail.com>
2022-10-16 11:46:18 +0700

    asdsa

--- install-mun.sh
+++ install-mun.sh
@@ -54,10 +54,50 @@ curl --tlsv1 https://node1.mun.money/genesis? | jq ".result.genesis" > ~/.mun/co
 sleep 1
 
 
-word1='seeds = ""'
-word2='seeds = "b4eeaf7ca17e5186b181885714cedc6a78d20c9b@167.99.6.48:26656,6a08f2f76baed249d3e3c666aaef5884e4b1005c@167.71.0.38:26656,9240277fca3bfa0c3b94efa60215ca10cf54f249@45.76.68.116:26656"'
-sed "s/$word1/$word2/g" ~/.mun/config/config.toml
+word1="seeds = \"\""
+word2='seeds = \"b4eeaf7ca17e5186b181885714cedc6a78d20c9b@167.99.6.48:26656,6a08f2f76baed249d3e3c666aaef5884e4b1005c@167.71.0.38:26656,9240277fca3bfa0c3b94efa60215ca10cf54f249@45.76.68.116:26656\"'
+sed -i "s@$word1@$word2@g" ~/.mun/config/config.toml
 
-word3='laddr = "tcp://127.0.0.1:34657"'
-word4='laddr = "tcp://0.0.0.0:34657"'
-sed "s/$word3/$word4/g" ~/.mun/config/config.toml
+word3="laddr = \"tcp://127.0.0.1:26657\""
+word4="laddr = \"tcp://0.0.0.0:26657\""
+sed -i "s@$word3@$word4@g" ~/.mun/config/config.toml
+sed -i 's/stake/utmun/g' ~/.mun/config/genesis.json
+
+sleep 1
+cat << 'EOF' >/etc/systemd/system/mund.service
+[Unit]
+Description=mund
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+Restart=on-failure
+RestartSec=3
+User=root
+Group=root
+Environment=DAEMON_NAME=mund
+Environment=DAEMON_HOME=/root/.mun
+Environment=DAEMON_ALLOW_DOWNLOAD_BINARIES=on
+Environment=DAEMON_RESTART_AFTER_UPGRADE=on
+PermissionsStartOnly=true
+ExecStart=/usr/bin/mund-manager start --pruning="nothing" --rpc.laddr "tcp://0.0.0.0:26657"
+StandardOutput=file:/var/log/mund/mund.log
+StandardError=file:/var/log/mund/mund_error.log
+ExecReload=/bin/kill -HUP $MAINPID
+KillSignal=SIGTERM
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+sleep 1
+make log-files
+
+sleep 1
+sudo systemctl enable mund
+sudo systemctl start mund
+
+sleep 1
+mund status
